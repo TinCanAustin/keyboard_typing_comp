5a2c4ec1397b320629b8c9eba6f85bb558e01a85
TinCanAustin <austin.bijumon@gmail.com>
2025-09-02 18:31:07 -0700

    Tracker calculation

--- src/components/typing/header.tsx
+++ src/components/typing/header.tsx
@@ -5,13 +5,34 @@ import { ChangeEvent, useState, useEffect, useRef } from 'react';
 import './style.css'
 
 export default function TypingCheck(){
-    const word = "I am so fucking tired of the small dick jokes. You guys think you are so funny. You think bigger is better. You are all morons. My dick is a precision instrument. A surgical tool. It is designed for accuracy not blunt force trauma. You guys are walking around with these giant unmanageable salamis. You probably knock over lamps when you turn around too fast. I hit the g-spot like a special forces sniper. Pinpoint accuracy every single time. You guys are just carpet bombing the whole area hoping to hit something. You are the collateral damage of sex. Women tell me they prefer it. They say its cute. They say it doesnt feel like their insides are being rearranged by a rogue battering ram. You are giving women internal bleeding and think you are a sex god. So laugh all you want at my tactical 3 inch stud finder. Ill be the one getting invited back. You will be wondering why she blocked your number after you gave her a prolapsed uterus with your goddamn anaconda. You probably dont even know what a clitoris is. You think its a type of dinosaur. Clitorisaurus Rex. Fucking idiots."
+    const word = "I am so fucking tired of the small dick jokes. You guys think you are so funny."
     
     const [input, setInput] = useState("");
     const [index, setIndex] = useState(0);
 
     const charRef = useRef<(HTMLSpanElement | null)[]>([]);
     const inputRef = useRef<(HTMLInputElement | null)>(null);
+
+    const [error, setError] = useState(0);
+
+    const [_timer, upTimer] = useState(0);
+    const [startState, setStartState] = useState(true); //true - can start, false - cannot start 
+    const timerManager = useRef<NodeJS.Timeout | null>(null);
+
+    //score
+    let grossWPM : number = 0;
+    let netWPM : number = 0;
+    let accuracy : number = 0;
+    let time : string = '';
+
+    const getTime = (totalTime: number) => {
+        const min = Math.floor(totalTime / 60);
+        const sec = totalTime - (min * 60);
+
+        const time = `${min}:${sec}`;
+        return time;
+    }
+    
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const val = e.target.value;
@@ -38,18 +59,44 @@ export default function TypingCheck(){
         }
     }
 
+    const calculateResults = ()=>{
+        //score eval
+        grossWPM = (index / 5) / (_timer / 60);
+        netWPM = (grossWPM - error) / (_timer / 60);
+        accuracy = ((index - error) / index) * 100;
+
+        //time eval
+        time = getTime(_timer);
+    }
+
     useEffect(()=>{
-        console.log(index);
         if(index > 0){
+            if(startState){
+                console.log("done");
+                setStartState(false);
+                timerManager.current = setInterval(() => {
+                    upTimer(prevT => prevT + 1);
+                }, 1000);
+            }
             if(charRef.current[index - 1]!.textContent == input[index - 1]){
                 charRef.current[index - 1]!.classList.add('textCorrect');
             }else{
                 charRef.current[index - 1]!.classList.add('textWrong');
+                setError(error + 1);
             }
         }else{
             charRef.current[0]!.className = '';
         }
     }, [index]);
+
+    useEffect(()=>{
+        console.log(_timer);
+        if(_timer >= 20 || index >= word.length){
+            if (timerManager.current) clearInterval(timerManager.current);
+            calculateResults();
+            console.log(grossWPM, netWPM, accuracy, time);
+        }
+    }, [_timer, index]);
 
     const focusText = ()=>{
         inputRef.current?.focus();
