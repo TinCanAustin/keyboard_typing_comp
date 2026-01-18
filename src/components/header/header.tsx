3f0018c7782b6d78e99a48fe01b5bbad5ca69115
TinCanAustin <austin.bijumon@gmail.com>
2026-01-17 19:20:06 -0800

    Header fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,12 +3,13 @@
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Header(){
     const { resolvedTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
     const pathname = usePathname();
+    const router = useRouter();
 
     useEffect(()=>{
         setMounted(true);
@@ -24,13 +25,13 @@ export default function Header(){
 
     return (
         <div className="header">
-            <a href="/"><Image
+            <button style={{cursor: "pointer"}} onClick={()=>{router.replace('/')}}><Image
                 src={resolvedTheme == "dark" ? "/placementLogo-Dark.png" : "/placementLogo-Light.png"}
                 alt="Logo"
                 width={300}
                 height={78}
                 priority={true}
-            /></a>
+            /></button>
         </div>
     );
 }
